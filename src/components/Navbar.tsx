dc1a667f745ac2b4e57d0b32fb3e35709a14d082
Ikonicity Airban <ikonicityairban@gmail.com>
2023-06-27 17:02:23 -0700

    unused param

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,67 +1,64 @@
-import { Avatar, Button, Navbar } from "flowbite-react";
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import LogoComponent from "./LogoComponent";
+import { Avatar, Navbar } from 'flowbite-react';
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import LogoComponent from './LogoComponent';
 
 function NavbarComponent() {
-  const navLinks = React.useMemo(
-    () => [
-      {
-        title: "Home",
-        link: "/",
-      },
-      {
-        title: "About",
-        link: "/about",
-      },
-      {
-        title: "Tours & Packages",
-        link: "/packages",
-      },
-      {
-        title: "Services",
-        link: "/services",
-      },
-    ],
-    []
-  );
+    const navLinks = React.useMemo(
+        () => [
+            {
+                title: 'Home',
+                link: '/',
+            },
+            {
+                title: 'About',
+                link: '/about',
+            },
+            {
+                title: 'Tours & Packages',
+                link: '/packages',
+            },
+            {
+                title: 'Services',
+                link: '/services',
+            },
+        ],
+        []
+    );
 
-  return (
-    <div className="md:container sticky w-full mx-auto p-4 bg-white md:p-6 top-0 rounded-lg shadow-lg overflow-hidden z-10 md:-my-10">
-      <Navbar
-        fluid
-        style={{
-          border: "none",
-        }}
-      >
-        <Navbar.Brand className="flex-1 items-end" href="/">
-          <LogoComponent />
-        </Navbar.Brand>
-        <Navbar.Toggle className="order-2" />
-        <Navbar.Collapse className="order-3">
-          {navLinks.map(({ link, title }) => (
-            <NavLink
-              className={({ isActive }) =>
-                isActive
-                  ? "relative text-primary overflow-hidden active-link py-1"
-                  : "overflow-hidden relative text-[#11224499] py-1"
-              }
-              to={link}
-              key={title}
+    return (
+        <div className="md:container sticky w-full mx-auto p-4 bg-white md:p-6 top-0 rounded-lg shadow-lg overflow-hidden z-10 md:-my-10">
+            <Navbar
+                fluid
+                style={{
+                    border: 'none',
+                }}
             >
-              <span className="link">{title}</span>
-            </NavLink>
-          ))}
-        </Navbar.Collapse>
-        <Link
-          to="/login"
-          className="cursor-pointer -my-1 px-4 md:order-3 order-0"
-        >
-          <Avatar rounded />
-        </Link>
-      </Navbar>
-    </div>
-  );
+                <Navbar.Brand className="flex-1 items-end" href="/">
+                    <LogoComponent />
+                </Navbar.Brand>
+                <Navbar.Toggle className="order-2" />
+                <Navbar.Collapse className="order-3">
+                    {navLinks.map(({ link, title }) => (
+                        <NavLink
+                            className={({ isActive }) =>
+                                isActive
+                                    ? 'relative text-primary overflow-hidden active-link py-1'
+                                    : 'overflow-hidden relative text-[#11224499] py-1'
+                            }
+                            to={link}
+                            key={title}
+                        >
+                            <span className="link">{title}</span>
+                        </NavLink>
+                    ))}
+                </Navbar.Collapse>
+                <Link to="/login" className="cursor-pointer -my-1 px-4 md:order-3 order-0">
+                    <Avatar rounded />
+                </Link>
+            </Navbar>
+        </div>
+    );
 }
 
 export default NavbarComponent;
--- src/components/PremiumCard.tsx
+++ src/components/PremiumCard.tsx
@@ -1,56 +1,55 @@
-import { Card } from "flowbite-react";
-import image from "../assets/react.svg";
+import image from '../assets/react.svg';
 
 type Props = {
-  title?: string;
-  rate?: string;
-  price?: string;
-  source?: string;
-  person?: number;
-  days?: number;
+    title?: string;
+    rate?: string;
+    price?: string;
+    source?: string;
+    person?: number;
+    days?: number;
 };
 
 function PremiumCard({
-  source = image,
-  price = "#45000",
-  rate = "4.5",
-  title = "Enugu",
-  days = 30,
-  person = 7,
+    source = image,
+    price = '#45000',
+    rate = '4.5',
+    title = 'Enugu',
+    days = 30,
+    person = 7,
 }: Props) {
-  return (
-    <div className="rounded card ring-2 ring-[#d4b400] overflow-hidden border bg-white mb-2 text-sm">
-      <img className="w-full" src={source} alt="" />
-      <div className="p-4">
-        <div className="flex justify-between mb-3">
-          <small className="m-0">
-            <i className="fa fa-map-marker-alt text-primary mr-2"></i>
-            {title}
-          </small>
-          <small className="m-0">
-            <i className="fa fa-calendar-alt text-primary mr-2"></i>
-            {days} days
-          </small>
-          <small className="m-0">
-            <i className="fa fa-user text-primary mr-2"></i>
-            {person} Person
-          </small>
+    return (
+        <div className="rounded card ring-2 ring-[#d4b400] overflow-hidden border bg-white mb-2 text-sm">
+            <img className="w-full" src={source} alt="" />
+            <div className="p-4">
+                <div className="flex justify-between mb-3">
+                    <small className="m-0">
+                        <i className="fa fa-map-marker-alt text-primary mr-2"></i>
+                        {title}
+                    </small>
+                    <small className="m-0">
+                        <i className="fa fa-calendar-alt text-primary mr-2"></i>
+                        {days} days
+                    </small>
+                    <small className="m-0">
+                        <i className="fa fa-user text-primary mr-2"></i>
+                        {person} Person
+                    </small>
+                </div>
+                <a className="text-decoration-none" href="">
+                    {}
+                </a>
+                <div className="border-t-2 mt-4 pt-4">
+                    <div className="flex justify-between">
+                        <h6 className="m-0">
+                            <i className="fa fa-star text-primary mr-2"></i>
+                            {rate} <small>(250)</small>
+                        </h6>
+                        <h5 className="m-0">{price}</h5>
+                    </div>
+                </div>
+            </div>
         </div>
-        <a className="text-decoration-none" href="">
-          {}
-        </a>
-        <div className="border-t-2 mt-4 pt-4">
-          <div className="flex justify-between">
-            <h6 className="m-0">
-              <i className="fa fa-star text-primary mr-2"></i>
-              {rate} <small>(250)</small>
-            </h6>
-            <h5 className="m-0">{price}</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default PremiumCard;
