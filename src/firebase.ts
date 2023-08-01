6bcc1a4ae7c0db41cd10dcc4f83991b2aafe3f7b
Ikonicity Airban <ikonicityairban@gmail.com>
2023-08-01 16:53:03 +0100

    firebaseConfig

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,30 +1,32 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
 import "firebase/firestore";
+
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB78xRVePuYrUsQscPc5OqDcMPIJ9UPUSU",
-//   authDomain: "tourism-f262a.firebaseapp.com",
-//   projectId: "tourism-f262a",
-//   storageBucket: "tourism-f262a.appspot.com",
-//   messagingSenderId: "67867833421",
-//   appId: "1:67867833421:web:e7a2918fbf223a251b5804",
-// };
-
 const firebaseConfig = {
+  apiKey: "AIzaSyB78xRVePuYrUsQscPc5OqDcMPIJ9UPUSU",
+  authDomain: "tourism-f262a.firebaseapp.com",
+  projectId: "tourism-f262a",
+  storageBucket: "tourism-f262a.appspot.com",
+  messagingSenderId: "67867833421",
+  appId: "1:67867833421:web:e7a2918fbf223a251b5804",
+};
+
+/* const firebaseConfig = {
   apiKey: "AIzaSyD7_E2ZQ968r_THrrktAsRvTDbF_h-GVho",
   authDomain: "estc-app.firebaseapp.com",
   projectId: "estc-app",
   storageBucket: "estc-app.appspot.com",
   messagingSenderId: "43708951651",
   appId: "1:43708951651:web:7579743a9da83e0d991c56",
-};
+}; */
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
